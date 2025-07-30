318eb52a8f966dd343a79a8a9bf15967b6f15edb
ZIN MYO SWE <overmidnight12@gmail.com>
2025-07-31 02:40:40 +0700

    Auth Puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,10 @@
 import type { Route } from "./+types/home";
+import Navbar from "~/components/Navbar ";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,7 +14,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+    <Navbar />
     
     <section className="main-section">
         <div className="page-heading py-16">
