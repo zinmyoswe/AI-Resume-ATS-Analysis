a46d0cf9ae49a22d9d07dc9f94e75dfecdb856ca
ZIN MYO SWE <overmidnight12@gmail.com>
2025-07-31 04:47:57 +0700

    Resume Feedback Component

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import ScoreGauge from "../components/ScoreGauge";
 
-const Summary = () => {
-    return (
-        <div>Summary</div>
-    )
-}
-export default Summary
+const ScoreBadge = ({ score }: { score: number }) => {
+  const badgeColor =
+    score > 69
+      ? "bg-badge-green"
+      : score > 49
+      ? "bg-badge-yellow"
+      : "bg-badge-red";
+  const textColor =
+    score > 69
+      ? "text-green-600"
+      : score > 49
+      ? "text-yellow-600"
+      : "text-red-600";
+  const badgeText =
+    score > 69 ? "Strong" : score > 49 ? "Good Start" : "Needs Work";
+
+  return (
+    <div className={`score-badge ${badgeColor}`}>
+      <p className={`text-xs ${textColor} font-semibold`}>{badgeText}</p>
+    </div>
+  );
+};
+
+const Category = ({ title, score }: { title: string; score: number }) => {
+  const textColor =
+    score > 69
+      ? "text-green-600"
+      : score > 49
+      ? "text-yellow-600"
+      : "text-red-600";
+
+  return (
+    <div className="resume-summary">
+      <div className="category">
+        <div className="flex flex-row gap-2 items-center justify-center">
+          <p className="text-2xl">{title}</p>
+          <ScoreBadge score={score} />
+        </div>
+        <p className="text-2xl ">
+          <span className={textColor}>{score}</span>/100
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Summary = ({ feedback }: { feedback: Feedback }) => {
+  return (
+    <div className="bg-white rounded-2xl shadow-md w-full">
+      <div className="flex flex-row max-sm:flex-col  items-center p-4 gap-8">
+        <ScoreGauge score={feedback.overallScore} />
+        <div className="flex flex-col gap-2">
+          <h2 className="text-2xl font-bold">Your Resume Score</h2>
+          <p className="text-sm text-gray-500">
+            This score is calculated based on the variables listed below.
+          </p>
+        </div>
+      </div>
+      <Category title="Tone & Style" score={feedback.toneAndStyle.score} />
+      <Category title="Content" score={feedback.content.score} />
+      <Category title="Structure" score={feedback.structure.score} />
+      <Category title="Skills" score={feedback.skills.score} />
+    </div>
+  );
+};
+
+export default Summary;
