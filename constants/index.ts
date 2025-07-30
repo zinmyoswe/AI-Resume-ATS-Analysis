1368a899b655bfc2c34c4fcc741ad67884e6d2eb
ZIN MYO SWE <overmidnight12@gmail.com>
2025-07-31 02:05:20 +0700

    Create HomePage,Navbar,ResumeCard

---
+++ constants/index.ts
@@ -0,0 +1,226 @@
+export const resumes: Resume[] = [
+    {
+        id: "1",
+        companyName: "Google",
+        jobTitle: "Frontend Developer",
+        imagePath: "/images/resume_01.png",
+        resumePath: "/resumes/resume-1.pdf",
+        feedback: {
+            overallScore: 85,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
+    {
+        id: "2",
+        companyName: "Microsoft",
+        jobTitle: "Cloud Engineer",
+        imagePath: "/images/resume_02.png",
+        resumePath: "/resumes/resume-2.pdf",
+        feedback: {
+            overallScore: 55,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
+    {
+        id: "3",
+        companyName: "Apple",
+        jobTitle: "iOS Developer",
+        imagePath: "/images/resume_03.png",
+        resumePath: "/resumes/resume-3.pdf",
+        feedback: {
+            overallScore: 75,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
+    {
+        id: "4",
+        companyName: "Google",
+        jobTitle: "Frontend Developer",
+        imagePath: "/images/resume_01.png",
+        resumePath: "/resumes/resume-1.pdf",
+        feedback: {
+            overallScore: 85,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
+    {
+        id: "5",
+        companyName: "Microsoft",
+        jobTitle: "Cloud Engineer",
+        imagePath: "/images/resume_02.png",
+        resumePath: "/resumes/resume-2.pdf",
+        feedback: {
+            overallScore: 55,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
+    {
+        id: "6",
+        companyName: "Apple",
+        jobTitle: "iOS Developer",
+        imagePath: "/images/resume_03.png",
+        resumePath: "/resumes/resume-3.pdf",
+        feedback: {
+            overallScore: 75,
+            ATS: {
+                score: 90,
+                tips: [],
+            },
+            toneAndStyle: {
+                score: 90,
+                tips: [],
+            },
+            content: {
+                score: 90,
+                tips: [],
+            },
+            structure: {
+                score: 90,
+                tips: [],
+            },
+            skills: {
+                score: 90,
+                tips: [],
+            },
+        },
+    },
+];
+
+export const AIResponseFormat = `
+      interface Feedback {
+      overallScore: number; //max 100
+      ATS: {
+        score: number; //rate based on ATS suitability
+        tips: {
+          type: "good" | "improve";
+          tip: string; //give 3-4 tips
+        }[];
+      };
+      toneAndStyle: {
+        score: number; //max 100
+        tips: {
+          type: "good" | "improve";
+          tip: string; //make it a short "title" for the actual explanation
+          explanation: string; //explain in detail here
+        }[]; //give 3-4 tips
+      };
+      content: {
+        score: number; //max 100
+        tips: {
+          type: "good" | "improve";
+          tip: string; //make it a short "title" for the actual explanation
+          explanation: string; //explain in detail here
+        }[]; //give 3-4 tips
+      };
+      structure: {
+        score: number; //max 100
+        tips: {
+          type: "good" | "improve";
+          tip: string; //make it a short "title" for the actual explanation
+          explanation: string; //explain in detail here
+        }[]; //give 3-4 tips
+      };
+      skills: {
+        score: number; //max 100
+        tips: {
+          type: "good" | "improve";
+          tip: string; //make it a short "title" for the actual explanation
+          explanation: string; //explain in detail here
+        }[]; //give 3-4 tips
+      };
+    }`;
---
+++ types/index.d.ts
@@ -0,0 +1,51 @@
+interface Resume {
+    id: string;
+    companyName?: string;
+    jobTitle?: string;
+    imagePath: string;
+    resumePath: string;
+    feedback: Feedback;
+}
+
+interface Feedback {
+    overallScore: number;
+    ATS: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+        }[];
+    };
+    toneAndStyle: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+            explanation: string;
+        }[];
+    };
+    content: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+            explanation: string;
+        }[];
+    };
+    structure: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+            explanation: string;
+        }[];
+    };
+    skills: {
+        score: number;
+        tips: {
+            type: "good" | "improve";
+            tip: string;
+            explanation: string;
+        }[];
+    };
+}
